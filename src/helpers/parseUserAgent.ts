35607a9b8bef4298838be318d4c6061002b9c250
Rashed <rashed@notionhive.com>
2025-06-20 15:24:38 +0600

    fix: deploy issue

--- src/helpers/parseUserAgent.ts
+++ src/helpers/parseUserAgent.ts
@@ -1,26 +1,21 @@
+import DeviceDetector from "node-device-detector";
+
 export const parseUserAgent = (userAgent: string) => {
-    const ua = userAgent.toLowerCase();
-  
-    let deviceType = 'desktop';
-    if (/mobile|android|iphone|ipad/i.test(ua)) {
-      deviceType = 'mobile';
-    } else if (/tablet|ipad/i.test(ua)) {
-      deviceType = 'tablet';
-    }
-  
-    let os = 'unknown';
-    if (/windows/i.test(ua)) os = 'Windows';
-    else if (/macintosh|mac os x/i.test(ua)) os = 'MacOS';
-    else if (/linux/i.test(ua)) os = 'Linux';
-    else if (/android/i.test(ua)) os = 'Android';
-    else if (/iphone|ipad|ios/i.test(ua)) os = 'iOS';
-  
-    let browser = 'unknown';
-    if (/firefox/i.test(ua)) browser = 'Firefox';
-    else if (/chrome|crios/i.test(ua)) browser = 'Chrome';
-    else if (/safari/i.test(ua)) browser = 'Safari';
-    else if (/edge/i.test(ua)) browser = 'Edge';
-    else if (/opera|opios/i.test(ua)) browser = 'Opera';
-  
-    return { deviceType, os, browser };
-  };
+  const detector = new DeviceDetector({
+    clientIndexes: true,
+    deviceIndexes: true,
+    osIndexes: true,
+    deviceAliasCode: false,
+    deviceTrusted: false,
+    deviceInfo: false,
+    maxUserAgentSize: 500,
+  });
+  // const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
+  const result = detector.detect(userAgent);
+  console.log('result parse', result);
+  console.log("agent ", userAgent)
+  const deviceType = "unknown"
+  const os = "unknown"
+  const browser = "unknown"
+  return { deviceType, os, browser };
+};
